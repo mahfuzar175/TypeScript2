0adbcfe5cc3bc34fad6b475e9ff8ea6dd3325571
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-04-08 19:01:13 +0600

    todo app created

--- my-react-app/src/index.tsx
+++ my-react-app/src/index.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { v4 as uuidv4 } from 'uuid';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+type Task ={
+  id: string
+  title: string
+  completed: boolean
+  createdAd: Date
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+form?.addEventListener("submit", e=>{
+  e.preventDefault()
+
+
+  if(input?.value === "" || input?.value == null)return
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value, 
+    completed: false,
+    createdAd: new Date(),
+  }
+tasks.push(newTask)
+saveTasks()
+
+addListItem(newTask)
+input.value = ""
+
+})
+
+function addListItem(task:Task){
+  const item = document.createElement("li")
+  const lable = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", ()=>{
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type="checkbox"
+  checkbox.checked = task.completed
+  lable.append(checkbox, task.title)
+  item.append(lable)
+  list?.append(item)
+
+}
+
+function saveTasks(){
+  localStorage.setItem("TASKS", JSON.stringify("TASKS"))
+}
+
+function loadTasks(): Task[]{
+  const taskJSON = localStorage.getItem("TASKS")
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
